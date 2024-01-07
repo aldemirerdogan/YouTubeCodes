fef3ad2a47d82e0bca5bb4cbd1d69f0c8e45c4de
Erdogan Aldemir <aldemir.erdogan@gmail.com>
2024-01-07 10:55:15 +0300

    Update degiskenler.c
    tablo karakterleri eklendi

--- degiskenler.c
+++ degiskenler.c
@@ -1,4 +1,6 @@
-/* Online C Compiler and Editor */
+/* yazan: E.aldemir
+    versiyon: v1.1
+*/
 // örnek:  tanımlayıcı ve değişkenler 
 #include <stdio.h> // temel giriş/çıkış kütüphanesi  
 
@@ -6,11 +8,13 @@ int main() {
     // Write C code here
     int degisken1 = 3, degisken2=5;
     float degiskenf1 = 3.0, degiskenf2=5.0; 
-    printf("Toplama : %d + %d = %d \n ", degisken1, degisken2, degisken1+degisken2);
-    printf("Çarpma : %d * %d = %d \n ", degisken1, degisken2, degisken1*degisken2);
-    printf("Bölme : %d / %d = %d \n ", degisken1, degisken2, degisken1/degisken2);
-    printf("Bölme (float) : %f / %f = %f \n ", degiskenf1, degiskenf2, degiskenf1/degiskenf2);
-    printf("Bölme (float) : %f / %f = %d \n ", degiskenf1, degiskenf2, degiskenf1/degiskenf2);
+    printf("====================== matematiksel işlemler ===================");
+    printf("|\t Toplama : %2d + %d = %d \t\t\t\t\t | \n ", degisken1, degisken2, degisken1+degisken2);
+    printf("|\t Çıkarma : %2d - %d = %d \t\t\t\t\t | \n ", degisken1, degisken2, degisken1-degisken2);
+    printf("|\t Çarpma : %2d * %d = %d \t\t\t\t\t  | \n ", degisken1, degisken2, degisken1*degisken2);
+    printf("|\t Bölme : %d / %d = %d \t\t\t\t\t  | \n ", degisken1, degisken2, degisken1/degisken2);
+    printf("|\t Bölme (float) : %f / %f = %f |\n ", degiskenf1, degiskenf2, degiskenf1/degiskenf2);
+    printf("| \t Bölme (float) : %f / %f = %d \t\t | \n ", degiskenf1, degiskenf2, degiskenf1/degiskenf2);
 
     return 0;
 }
